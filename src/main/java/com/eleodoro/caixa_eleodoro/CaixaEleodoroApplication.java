f9f3a867613840fb72ff07c3d8244bbfd9679afd
Zecapaugordinho69 <joas.martins@escola.pr.gov.br>
2024-08-06 12:12:16 -0300

    qualquer coisa

---
+++ src/main/java/com/eleodoro/caixa_eleodoro/CaixaEleodoroApplication.java
@@ -0,0 +1,15 @@
+package com.eleodoro.caixa_eleodoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@SpringBootApplication
+public class CaixaEleodoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaixaEleodoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eleodoro/caixa_eleodoro/CaixaEleodoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleodoro.caixa_eleodoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaixaEleodoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
