2aa16888d0819741f886b97424c09ecdf1a88308
Capuzitos6 <c.monteiro@escola.pr.gov.br>
2024-08-12 11:58:13 -0300

    criei a classe Conta

---
+++ src/main/java/com/eleodoro/caixa_eleodoro/modelo/Conta.java
@@ -0,0 +1,91 @@
+package com.eleodoro.caixa_eleodoro.modelo;
+
+public class Conta {
+    
+    private String nome;
+    private String descricao;
+    
+    
+    public Conta() {
+    }
+
+
+    public Conta(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void registrarEntrada(){
+
+    }
+
+    public void registrarSaida(){
+
+    }
+
+    public void verificarOperacaoExistente(){
+
+    }
+
+ 
+    @Override
+    public String toString() {
+        return "Conta [nome=" + nome + ", descricao=" + descricao + "]";
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Conta other = (Conta) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (descricao == null) {
+            if (other.descricao != null)
+                return false;
+        } else if (!descricao.equals(other.descricao))
+            return false;
+        return true;
+    }
+
+    
+    
+}
