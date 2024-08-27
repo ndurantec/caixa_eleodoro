ad9dcb9703dc4c6b63d8e5b656cfde732cc06f63
Zecapaugordinho69 <joas.martins@escola.pr.gov.br>
2024-08-27 11:23:17 -0300

    classe gestão de recuperação

---
+++ src/main/java/com/eleodoro/caixa_eleodoro/modelo/Gestao.java
@@ -0,0 +1,85 @@
+package com.eleodoro.caixa_eleodoro.modelo;
+
+public class Gestao {
+
+    private String nome;
+    private String senha;
+    private String email;
+    
+    public Gestao() {
+    }
+    
+    public Gestao(String nome, String senha, String email) {
+        this.nome = nome;
+        this.senha = senha;
+        this.email = email;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getSenha() {
+        return senha;
+    }
+    
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((senha == null) ? 0 : senha.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        return result;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Gestao other = (Gestao) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (senha == null) {
+            if (other.senha != null)
+                return false;
+        } else if (!senha.equals(other.senha))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        return true;
+    }
+    
+    @Override
+    public String toString() {
+        return "Gestao [nome=" + nome + ", senha=" + senha + ", email=" + email + "]";
+    }
+
+
+}
